cae526b9aaf2d6a8e32f9635df46fa63bc4df509
BISHAL NANDY <59905645+nandybishal23@users.noreply.github.com>
2021-04-17 18:39:51 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import PIL.ImageGrab
+import datetime
+from tkinter import *
+import time
+
+root = Tk()
+
+root.title("Screen_Shot")
+
+def take_ss():
+    root.withdraw()
+    time.sleep(1)
+    img=PIL.ImageGrab.grab()
+    img.save("Output.png")
+    root.withdraw()
+
+    root.update()
+    root.deiconify()    
+
+l1=Label(root,text='ScreenShoot Taker',width=30,bg='coral',fg='white',font="time 20")
+l1.grid(row=1,column=0,columnspan=2,padx=10,pady=10)
+btn1=Button(root, text='Take Shot',font="time 20",width=15,bg='black',fg='gold',command=take_ss,activebackground="gold",activeforeground="black")
+btn1.grid(row=2,column=0,columnspan=2,padx=10,pady=10)
+root.mainloop()
