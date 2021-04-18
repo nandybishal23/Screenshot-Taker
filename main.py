cbf5bac3e17332864fde39b63f950e9784941fb0
BISHAL NANDY <59905645+nandybishal23@users.noreply.github.com>
2021-04-18 09:54:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def take_ss():
     root.update()
     root.deiconify()    
 
-l1=Label(root,text='ScreenShoot Taker',width=30,bg='coral',fg='white',font="time 20")
+l1=Label(root,text='ScreenShot Taker',width=30,bg='coral',fg='white',font="time 20")
 l1.grid(row=1,column=0,columnspan=2,padx=10,pady=10)
 btn1=Button(root, text='Take Shot',font="time 20",width=15,bg='black',fg='gold',command=take_ss,activebackground="gold",activeforeground="black")
 btn1.grid(row=2,column=0,columnspan=2,padx=10,pady=10)
